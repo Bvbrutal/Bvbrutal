0f7606a0bfdc8dc951ad387cfe466433b51e0f1b
psh <1023092893@qq.com>
2023-08-01 13:39:01 +0800

    Update remove_emojis.py

--- remove_emojis.py
+++ remove_emojis.py
@@ -1,15 +1,17 @@
-remove_file_path='README.md'
-simple='previous_output.md'
+remove_file_path = 'README.md'
+simple = 'previous_output.md'
 try:
-    with open (remove_file_path,'r',encoding='utf-8') as f1:
-        data1=f1.read()
-        
-    with open (simple,'r',encoding='utf-8') as f2:
-        data2=f2.read()
-        data1=data1.replace(data2,'')
-        
-    with open (remove_file_path,'w',encoding='utf-8') as f3:
+    with open(remove_file_path, 'r', encoding='utf-8') as f1:
+        data1 = f1.read()
+
+    with open(simple, 'r', encoding='utf-8') as f2:
+        data2 = f2.read()
+        data1 = data1.replace(data2, '')
+
+    with open(remove_file_path, 'w', encoding='utf-8') as f3:
         f3.write(data1)
+
+    print("succssfully delect :\n", data2)
 
 except FileNotFoundError:
     print("Error: One or both files not found.")
