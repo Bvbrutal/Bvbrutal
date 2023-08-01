d356bc4b410b9af214ad53c72e3541fcfe11afed
psh <1023092893@qq.com>
2023-08-01 11:13:12 +0800

    Update remove_emojis.py

--- remove_emojis.py
+++ remove_emojis.py
@@ -1,22 +1,15 @@
-import re
+remove_file_path='README.md'
+simple='previous_output.md'
+try:
+    with open (remove_file_path,'r',encoding='utf-8') as f1:
+        data1=f1.read()
+        with open (simple,'r',encoding='utf-8') as f2:
+            data2=f2.read()
+            data1=data1.replace(data2,'')
+            with open (remove_file_path,'w',encoding='utf-8') as f3:
+                f3.write(data1)
 
-def remove_special_emojis(markdown_text):
-    # 定义表情包的正则表达式模式
-    emoji_pattern = r':[a-zA-Z0-9_\-]+:'
-
-    # 使用re.sub()函数删除所有匹配的表情包
-    cleaned_text = re.sub(emoji_pattern, '', markdown_text)
-
-    return cleaned_text
-
-if __name__ == '__main__':
-    import sys
-
-    # 从标准输入中读取README.md的内容
-    readme_content = sys.stdin.read()
-
-    # 删除特殊表情包
-    cleaned_content = remove_special_emojis(readme_content)
-
-    # 输出删除表情包后的文本
-    print(cleaned_content)
+except FileNotFoundError:
+    print("Error: One or both files not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
