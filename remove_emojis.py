aa14cf056f8b5290ac2e97f136c2642f47639737
psh <1023092893@qq.com>
2023-08-01 11:35:08 +0800

    Update remove_emojis.py

--- remove_emojis.py
+++ remove_emojis.py
@@ -3,11 +3,13 @@ simple='previous_output.md'
 try:
     with open (remove_file_path,'r',encoding='utf-8') as f1:
         data1=f1.read()
-        with open (simple,'r',encoding='utf-8') as f2:
-            data2=f2.read()
-            data1=data1.replace(data2,'')
-            with open (remove_file_path,'w',encoding='utf-8') as f3:
-                f3.write(data1)
+        
+    with open (simple,'r',encoding='utf-8') as f2:
+        data2=f2.read()
+        data1=data1.replace(data2,'')
+        
+    with open (remove_file_path,'w',encoding='utf-8') as f3:
+        f3.write(data1)
 
 except FileNotFoundError:
     print("Error: One or both files not found.")
